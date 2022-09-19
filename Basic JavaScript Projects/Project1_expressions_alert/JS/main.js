c20feb7ef15106cdf6cb4c4789f27a40868cc6a2
aamasse16 <112958171+aamasse16@users.noreply.github.com>
2022-09-19 14:26:26 -0400

    Update main.js
    added much more js to my main.js file

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,9 @@
-alert("This is Aarons site!")
+window.alert("Hey welcome to my JS learning site!")//telling th browser to use an alert box when the website is first accessed and to print this text in the box
+
+var Sent1 = "This is the beginning of the string, ", Sent2 = "and this is the end of the string."//creating 2 strings
+
+document.write(Sent1 + Sent2)//Concatenating the 2 strings above
+
+var A = 5, B = 6, C = 7 //creating 3 variables
+
+document.write(C + B) //telling the browser to print the sum of C and B (and expression)
